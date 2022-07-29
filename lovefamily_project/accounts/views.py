3026cf82b6f39e7268c3a663fa82ed7aa707948f
FOTSI2022 <dufotsi2020@gmail.com>
2022-07-29 14:16:08 +0100

    vendredi

--- lovefamily_project/accounts/views.py
+++ lovefamily_project/accounts/views.py
@@ -98,44 +98,6 @@ def login(request):
 def update_profile(request):
     g=['male', 'female']
     if request.method=="POST":
-        
-        #get form values
-        if request.POST['phone_number']:
-                phone_number=request.POST['phone_number']
-        
-        if request.POST['occupation']:
-                occupation=request.POST['occupation']
-        
-        if request.POST['father_last_name']:
-                father_last_name=request.POST['father_last_name']
-        
-        if request.POST['father_first_name']:
-                father_first_name=request.POST['father_first_name']
-        
-        if request.POST['mother_last_name']:
-                mother_last_name=request.POST['mother_last_name']
-        
-        if request.POST['mother_first_name']:
-                mother_first_name=request.POST['mother_first_name']
-        
-        if request.POST['birth_country']:
-                birth_country=request.POST['birth_country']
-        
-        if request.POST['birth_region']:
-                birth_region=request.POST['birth_region']
-       
-        if request.POST['history']:
-                history=request.POST['history']
-        
-        if request.POST.get('gender') :
-                gender=request.POST['gender']
-        
-        if request.FILES.get('profile_image'):
-                profile_image=request.FILES['profile_image']
-        
-        if request.POST['birth_date']:
-                birth_date=request.POST['birth_date']
-
         p=''       
         #On verifie que le user est dans la table person
         try:
@@ -148,7 +110,7 @@ def update_profile(request):
                 p.birth_date=request.POST['birth_date']
 
             if request.POST['phone_number']:
-                p.phone_number=request.POST['phone_number']
+                p.phone_number= request.POST['phone_number'] if request.POST.get('phone_number') else None  
             
             if request.POST['occupation']:
                 p.occupation=request.POST['occupation']
@@ -162,18 +124,6 @@ def update_profile(request):
             if request.POST['history']:
                 p.history=request.POST['history']
             
-            if request.POST['father_last_name']:
-                p.father_last_name=request.POST['father_last_name']
-        
-            if request.POST['father_first_name']:
-                p.father_first_name=request.POST['father_first_name']
-        
-            if request.POST['mother_last_name']:
-                    p.mother_last_name=request.POST['mother_last_name']
-        
-            if request.POST['mother_first_name']:
-                    p.mother_first_name=request.POST['mother_first_name']
-            
             if request.POST.get('gender') :
                 p.gender=request.POST['gender']
            
@@ -183,78 +133,106 @@ def update_profile(request):
             p.personallyregister=False
 
             #on met à jour les infos         
-            p.save()
+            
             
             #on verifie que son père est enregistré dans la table user
             try:
-                fatherUser=User.objects.filter(first_name=father_first_name, last_name=father_last_name)
+                fatherUser=User.objects.filter(first_name=request.POST['father_first_name'], last_name=request.POST['father_last_name']).first()
             except:
                 print("Le nom du père n'est pas enregistré")
             
             #Si le père n'est pas enregistré on l'enregistre
             if fatherUser is None:
-                fatherUser=User.objects.create(first_name=father_first_name, last_name=father_last_name, username=father_last_name+'pere')
+                fatherUser=User.objects.create(first_name=request.POST['father_first_name'], last_name=request.POST['father_last_name'], username=request.POST['father_last_name']+'pere')
                 fatherUser.set_password('0000')
                 fatherUser.save()
+
+            #print(request.POST['father_first_name'])
+            #print(fatherUser)
+            #print(type(fatherUser))
+            p.father = fatherUser
             #on verifie que sa mère est enregistré dans la table user
             try:
-                motherUser=User.objects.filter(first_name=mother_first_name, last_name=mother_last_name)
+                motherUser=User.objects.filter(first_name=request.POST['mother_first_name'], last_name=request.POST['mother_last_name']).first()
             except:
                 print("Le nom de la mère n'est pas enregistré")
 
             #Si la mère n'est pas enregistré on l'enregistre
             if motherUser is None:
-                motherUser=User.objects.create(first_name=mother_first_name, last_name=mother_last_name, username=mother_last_name+'mere')
+                motherUser=User.objects.create(first_name=request.POST['mother_first_name'], last_name=request.POST['mother_last_name'], username=request.POST['mother_last_name']+'mere')
                 motherUser.set_password('0000')
                 motherUser.save()          
-
+            p.mother =motherUser
+            
+            p.save()
             messages.success(request,'vos informations ont été mises à jour')
-            return redirect('index')
+            return redirect('profile')
             
         #le user n'est pas dans la table person
         else:
             #on verifie que son père est enregistré dans la table user
+            fatherUser=''
             try:
-                fatherUser=User.objects.filter(first_name=father_first_name, last_name=father_last_name)
+                fatherUser=User.objects.filter(first_name=request.POST['father_first_name'], last_name=request.POST['father_last_name']).first()
             except:
                 print("Le nom du père n'est pas enregistré")
             
             #Si le père n'est pas enregistré on l'enregistre
-            if fatherUser is None:
-                fatherUser=User.objects.create(first_name=father_first_name, last_name=father_last_name, username=father_last_name+'pere')
+            if not fatherUser :
+                fatherUser=User.objects.create(first_name=request.POST['father_first_name'], last_name=request.POST['father_last_name'], username=request.POST['father_last_name']+'pere')
                 fatherUser.set_password('0000')
                 fatherUser.save()
             #on verifie que sa mère est enregistré dans la table user
+            motherUser=''
             try:
-                motherUser=User.objects.filter(first_name=mother_first_name, last_name=mother_last_name)
+                motherUser=User.objects.filter(first_name=request.POST['mother_first_name'], last_name=request.POST['mother_last_name']).first()
             except:
                 print("Le nom de la mère n'est pas enregistré")
 
             #Si la mère n'est pas enregistré on l'enregistre
-            if motherUser is None:
-                motherUser=User.objects.create(first_name=mother_first_name, last_name=mother_last_name, username=mother_last_name+'mere')
+            if not motherUser :
+                motherUser=User.objects.create(first_name=request.POST['mother_first_name'], last_name=request.POST['mother_last_name'], username=request.POST['mother_last_name']+'mere')
                 motherUser.set_password('0000')
                 motherUser.save() 
 
+
             #on crèe la personne         
-            p= Person.objects.create(
+            p= Person(
                         user=request.user,                                                         
                         father=fatherUser,
-                        mother=motherUser,
-                        gender=gender,
-                        birth_date=birth_date,
-                        phone_number=phone_number,
-                        occupation=occupation,
-                        birth_country=birth_country,
-                        birth_region=birth_region,
-                        history=history,
-                        profile_image=profile_image     
-        
+                        mother=motherUser,   
                         )
-             
+            #get form values
+            if request.POST['phone_number'] and request.POST['phone_number'] != None :
+                    phone_number=request.POST['phone_number']
+                    p.phone_number= phone_number 
+
+            if request.POST['gender'] and request.POST['phone_number'] != None :
+                    gender=request.POST['gender']
+                    p.gender= gender 
+            if request.POST['birth_date'] and request.POST['birth_date'] != None :
+                    birth_date=request.POST['birth_date']
+                    p.birth_date= birth_date 
+            if request.POST['occupation'] and request.POST['occupation'] != None :
+                occupation=request.POST['occupation']
+                p.occupation= occupation            
+            if request.POST['birth_country'] and request.POST['birth_country'] != None :
+                    birth_country=request.POST['birth_country']
+                    p.birth_country= birth_country 
+            if request.POST['birth_region'] and request.POST['birth_region'] != None :
+                    birth_region=request.POST['birth_region']
+                    p.birth_region= birth_region 
+            if request.POST['history'] and request.POST['history'] != None :
+                    history=request.POST['history']
+                    p.history= history 
+            if request.POST['profile_image'] and request.POST['profile_image'] != None :
+                    profile_image=request.POST['profile_image']
+                    p.profile_image= profile_image  
+
+            p.save()
                 
-        messages.success(request,'vos informations sont créées')
-        return redirect('index')
+            messages.success(request,'vos informations sont créées')
+            return redirect('profile')
     else:
         messages.error(request, "Une erreur s'est produite")
         return render(request, 'accounts/update_profile.html',{'g':g})
